12d7d71712c02e9ed83feeaefcb1a59a7a896593
tuydenna <dennaluck12@gmail.com>
2025-08-13 15:08:44 +0700

    rm unused MS

--- src/config/middlewares/cores.middleware.ts
+++ src/config/middlewares/cores.middleware.ts
@@ -3,7 +3,7 @@ import cores from "cors";
 export function coresMiddleware() {
     return cores({
         origin: function (url, cb) {
-            const whiteList = ["http://localhost:3000", "http://139.99.54.113:3000"]
+            const whiteList = ["http://localhost:3000", "http://localhost:4000", "http://139.99.54.113:3000"]
             if (whiteList.indexOf(url) === -1 && url) {
                 return cb(new Error("Origin is not a valid URL"));
             }
--- src/lib/message-queue/mq-connector.ts
+++ src/lib/message-queue/mq-connector.ts
@@ -1,6 +1,7 @@
 import mqlib, {Channel} from "amqplib";
 import env from "dotenv";
 import SysLog from "@lib/logger/sys-log";
+import {getEnv} from "@utils/index";
 env.config();
 
 const exchangeKey: string = 'FRAGMENT_UPLOAD';
@@ -8,13 +9,12 @@ let MQ: Channel;
 
 async function connectRabbitMQ() {
     try {
-        const connection = await mqlib.connect(process.env.RABBITMQ_URL);
+        const connection = await mqlib.connect(getEnv("RABBITMQ_URL"));
         MQ = await connection.createChannel();
         await MQ.assertExchange(exchangeKey, "fanout", {durable: false});
         SysLog.success("[MQ Service]", "is connected successfully.");
     } catch (e) {
         SysLog.error("[MQ Service]", "failed to connect MQ Service!");
-
     }
 }
 
