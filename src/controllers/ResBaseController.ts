0deb02d8bc65c20bd038f888687c918038d37017
tuydenna <dennaluck12@gmail.com>
2025-09-16 15:51:01 +0700

    Add Update Profile API in UserController.ts

--- src/controllers/ResBaseController.ts
+++ src/controllers/ResBaseController.ts
@@ -4,7 +4,7 @@ import ErrorException from "@config/error/error-exception";
 
 export default class ResBaseController {
 	resSuccess(response: Response, data: any) {
-		return response.json({data, message: "success" });
+		return response.status(200).json({data, message: "success" });
 	}
 
 	resError(response: Response, {message, code }: ErrorException) {
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from 'express';
 import db from "@lib/prisma/db-connector";
-import {faker} from "@faker-js/faker";
-import {Put, Get, Post, Prefix, Res} from "express-router-controller-khmer";
+import {Put, Get, Post, Prefix, Res, Req, Body} from "express-router-controller-khmer";
 import UserService from "@services/UserService";
 import ResBaseController from "@controllers/ResBaseController";
+import fs, {WriteStream} from "fs";
+import storageEngine from "@services/StorageEngine";
+import {avatar_path, getFilePath, getStorageLink} from "@constant/path";
+import {User} from "@prisma/client";
+import SysLog from "@lib/logger/sys-log";
 
 @Prefix("/api/users")
-export default class UserController extends ResBaseController{
+export default class UserController extends ResBaseController {
 
 	private readonly service: UserService = new UserService();
 
@@ -24,18 +28,41 @@ export default class UserController extends ResBaseController{
 	
 	@Post('/')
 	async create(req: Request, res: Response) {
-		return this.resSuccess(res,  await this.service.create(req.body))
+		return this.resSuccess(res,  await this.service.create(req.body));
 	}
 
 	@Put('/:id')
-	async update(req: Request, res: Response) {
-		const user = await db.user.update({
-			where: {id: req.params.id},
-			data: {
-				name: faker.person.fullName(),
-				email: faker.internet.email()
-			}
-		});
-		return res.send(user);
+	async update(@Body() data, @Req() req: Request, @Res() res: Response) {
+		try {
+			const userId: string = req.params.id;
+			const user: User = await db.user.findFirst({
+				where: { id: userId }
+			});
+
+			req.headers["file-extension"] = "png";
+
+			const {src, fileName} = getFilePath(req, avatar_path);
+			const updatedUser: User = await new Promise((resolve, reject) =>  {
+				const writeStream: WriteStream = fs.createWriteStream(src);
+				writeStream.write(Buffer.from(data.file.replace("data:image/png;base64,", ""), "base64"));
+				writeStream.end()
+
+				writeStream.on("finish", async () => {
+					if (fs.existsSync(getStorageLink(user.avatar))) {
+						storageEngine.remove(getStorageLink(user.avatar));
+					}
+					await db.user.update({
+						where: {id: userId},
+						data: {avatar: fileName}
+					});
+					user.avatar = fileName;
+					resolve(user);
+				});
+			})
+			return this.resSuccess(res, updatedUser);
+		} catch (error) {
+			SysLog.error("[update users]", error);
+			this.resError(res, error)
+		}
 	}
 }
