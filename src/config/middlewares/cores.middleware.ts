043a6095d03e28fafe02c9d089381ae281bab4a1
tuydenna <dennaluck12@gmail.com>
2026-08-17 16:01:18 +0700

    update cors middleware

--- src/config/middlewares/cores.middleware.ts
+++ src/config/middlewares/cores.middleware.ts
@@ -1,14 +1,18 @@
 import cores from "cors";
 import SysLog from "@lib/logger/sys-log";
+import {getEnv} from "@utils/index";
 
 export function coresMiddleware() {
     return cores({
-        origin: function (url, cb) {
-            const whiteList = ["http://localhost:3000", "http://192.168.1.37:3000", "http://139.99.1.1:3000"]
-            if (whiteList.indexOf(url) === -1 && url) {
+        origin: function (url: string, cb: Function) {
+            if (getEnv("NODE_ENV") !== "production" || !url) {
+                return cb(null, true);
+            }
+            const whiteList: string[] = getEnv("CORS_ORIGIN")?.split(",") || [];
+            if (whiteList.indexOf(url) === -1) {
+                SysLog.error("[coresMiddleware]", url);
                 return cb(new Error("Origin is not a valid URL"));
             }
-            SysLog.error("[coresMiddleware]", url);
             return cb(null, true);
         } ,
         credentials: true
--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,7 @@ async function Server() {
     app.set('views', path.join(process.cwd(), 'src/views'));
     app.set('view engine', 'ejs');
 
-    app.use(express.json({limit: '5mb'}));
+    app.use(express.json({limit: '20mb'}));
     app.use(express.urlencoded({ extended: false}));
     app.use(cookieParser());
     app.use(compression());
